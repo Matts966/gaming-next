498a59fdbb69fee9b301025cc67efe09f7615424
Matts966 <Matts966@users.noreply.github.com>
2020-08-16 00:19:58 +0900

    Add statics

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 // Add style manually
 import 'react-upload-gallery/dist/style.css' // or scss
+import "./gallery.css";
 
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
