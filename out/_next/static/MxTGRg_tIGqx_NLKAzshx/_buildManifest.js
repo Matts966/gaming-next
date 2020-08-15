8ea82ee6857ce73e71bdf9adb75b1cc395488821
Matts966 <Matts966@users.noreply.github.com>
2020-08-16 01:40:11 +0900

    Use axios and image param

---
+++ out/_next/static/MxTGRg_tIGqx_NLKAzshx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-035ed7cc9871a20fcbe8.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-4a42d8e75f5001618283.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,50 @@
 import { NextPage } from 'next';
 import { Composite } from 'react-composite';
 import RUG from 'react-upload-gallery'
+import axios from 'axios'
+
+function customRequest({ uid, file, send, action, headers, onProgress, onSuccess, onError }) {
+    const form = new FormData();
+
+    // send file 
+    form.append('image', file)
+
+    const CancelToken = axios.CancelToken
+    const source = CancelToken.source()       
+
+    axios.post(
+        action,
+        form,
+        {
+            onUploadProgress: ({ total, loaded }) => {
+                onProgress(uid, Math.round(loaded / total * 100));
+            },
+            cancelToken: source.token
+        }
+    ).then(({ data: response }) => {
+        onSuccess(uid, response);
+    })
+    .catch(error => {
+        onError(uid, {
+            action,
+            status: error.request,
+            response: error.response
+        })
+    })
+    
+    return {
+        abort() {
+            source.cancel()
+        }
+    }
+}
 
 const IndexPage: NextPage = () => (
     <Composite>
         <h1>げーみんぐ hoge じぇねれーた</h1>
         <RUG
             action="/server/segmentation" // upload route
-            source={ response => response }
+            customRequest={customRequest}
         />
     </Composite>
 );
