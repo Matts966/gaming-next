8eb3b8801828783bacce13236ffb56cf6e57b96d
Matts966 <Matts966@users.noreply.github.com>
2020-08-16 14:09:50 +0900

    Fix share statement

---
+++ out/_next/static/3B1SGpEjCa3MRpZr2_kfG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-23f2c2ad48724bd382aa.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-4a42d8e75f5001618283.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
